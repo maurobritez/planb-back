93456be95f73e0979b4433888b2b909ffd84c465
mauro <mauro@mauro.mauro>
2024-05-12 18:17:30 -0300

    cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { envs } from './config/dotenv-config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   await app.listen(envs.PORT);
 }
 bootstrap();
